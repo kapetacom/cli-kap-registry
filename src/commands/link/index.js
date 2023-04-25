81dd33c6464aa5cf88c396ef43b36c4824098f7d
Henrik Hofmeister <henrik@newdawn.dk>
2023-04-25 14:13:55 +0200

    Moved most logic into shared module

--- src/commands/link/index.js
+++ src/commands/link/index.js
@@ -1,17 +1,6 @@
-const YAML = require('yaml');
-const Path = require("node:path");
-const FS = require("node:fs");
-const FSExtra = require('fs-extra');
-const ClusterConfiguration = require('@kapeta/local-cluster-config');
-const CLIHandler = require("../../handlers/CLIHandler");
+const CLIHandler = require('../../CLIHandler');
+const {Actions} = require('@kapeta/nodejs-registry-utils');
 
-function makeSymLink(directory, versionTarget) {
-    if (FS.existsSync(versionTarget)) {
-        FSExtra.removeSync(versionTarget);
-    }
-    FSExtra.mkdirpSync(Path.dirname(versionTarget));
-    FSExtra.createSymlinkSync(directory, versionTarget);
-}
 
 /**
  *
@@ -19,31 +8,6 @@ function makeSymLink(directory, versionTarget) {
  * @returns {Promise<void>}
  */
 module.exports = async function link(source) {
-    if (!source) {
-        source = process.cwd();
-    }
-
     const cli = CLIHandler.get(false);
-
-    const kapetaYmlFilePath = Path.join(source, 'kapeta.yml');
-    if (!FS.existsSync(kapetaYmlFilePath)) {
-        throw new Error('Current working directory is not a valid kapeta asset. Expected a kapeta.yml file');
-    }
-
-    const assetInfos = YAML.parseAllDocuments(FS.readFileSync(kapetaYmlFilePath).toString())
-        .map(doc => doc.toJSON());
-
-    //If there are multiple assets in the kapeta.yml - we still just create 1 symlink since both will
-    //otherwise be loaded twice
-    const assetInfo = assetInfos[0];
-    const [handle, name] = assetInfo.metadata.name.split('/');
-    const target = ClusterConfiguration.getRepositoryAssetPath(handle, name, 'local');
-    makeSymLink(source, target);
-
-    assetInfos.forEach(blockInfo => {
-        cli.info('Linked asset %s:local\n  %s --> %s', blockInfo.metadata.name, source, target);
-    })
-
-    await cli.check('Linking done', true);
-
+    return Actions.link(cli, source);
 };
